afe3e83d86de4c6face337d8aed1186daef9ba5a
ashnachadha <ashnachadha@gmail.com>
2020-01-23 16:40:42 -0800

    Add revised main with c-echo.h

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include "c-echo.h"
 
-<<<<<<< HEAD
 int main(int argv, char** argc) {
-	std::cout << echo(argv, argc);
-	std::cout << "\n";
-=======
-int main()
-{
-    std::string name;
-    std::cout << "What is your name?" << std::endl;
-    std::cin >> name;
-    std::cout << "Hello " << name << "!" << std::endl;
+	std::cout << "Count of output: " << count(echo(argv, argc)) << std::endl;
 
-    return 0;
->>>>>>> 9e1586a5a531438260ebd8e76b0851239f1a7873
+//int main()
+//{
+//    std::string name;
+//    std::cout << "What is your name?" << std::endl;
+//    std::cin >> name;
+//    std::cout << "Hello " << name << "!" << std::endl;
+
+   	 return 0;
 }
