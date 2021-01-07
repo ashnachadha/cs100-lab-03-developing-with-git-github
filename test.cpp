305b70cced545d84313cbe8aee5ae1cef420ddb6
ashnachadha <ashnachadha@gmail.com>
2021-01-06 18:12:25 -0800

    add lab 3 files

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include "c-echo.h"
+#include "c-count.h"
 
 #include "gtest/gtest.h"
 
@@ -25,6 +26,21 @@ TEST(EchoTest, Sentence) {
 TEST(EchoTest, NumbersAndSpecialChars) {
 	char* test_val[4]; test_val[0] = "./c-echo"; test_val[1] = "123456789"; test_val[2] = "!@#$%^&&"; test_val[3] = "1!2@3#4$5%6^7&8*";
 	EXPECT_EQ("123456789 !@#$%^&& 1!2@3#4$5%6^7&8*", echo(4, test_val));
+}
+
+TEST(CountTest, HelloWorld) {
+	std::string test_str = "hello world";
+	EXPECT_EQ(2, count(test_str));
+}
+
+TEST(CountTest, EmptyString) {
+	std::string test_str = "";
+	EXPECT_EQ(0, count(test_str));
+}
+
+TEST(CountTest, ManySpaces) {
+	std::string test_str = "  this  string has   weird  spacing";
+	EXPECT_EQ(5, count(test_str));
 }
 
 int main(int argc, char **argv) {
